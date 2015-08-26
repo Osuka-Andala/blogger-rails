d4097a8594c766cd47520f49c233896dedf8bb1e
Osuka-Andala <teresa.andala@gmail.com>
2015-08-26 14:55:09 +0300

    Modified the destroy method, adding the redirection

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,6 +26,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
+    redirect_to article_path(@article)
   end
 
 end
